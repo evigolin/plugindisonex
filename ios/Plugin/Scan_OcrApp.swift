28a1a480a2b301e3ecdd20b70fa6921cb174a429
androidvt <email@address.com>
2022-02-23 12:22:19 -0500

    Plugin OCR-SCAN

---
+++ ios/Plugin/Scan_OcrApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scan_OcrApp.swift
+//  Scan-Ocr
+//
+//   Created by David Blanco For VirtualTec
+//
+
+import SwiftUI
+
+@main
+struct Scan_OcrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //ContentView()
+        }
+    }
+}
